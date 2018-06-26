8f2c883c652baec45220935dc87c8add58a49946
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2018-06-26 22:11:07 +0100

    some working simple application

--- src/app.rs
+++ src/app.rs
@@ -1,2 +1,42 @@
+extern crate lib;
+use lib::arith::*;
+use lib::arith::Command::*;
+
+extern crate serde;
+extern crate serde_json;
+
 fn main() {
+    let args = [
+        "-cp",
+        "../tapl-scala/jvm/target/scala-2.12/taplScala-assembly-0.1.0-SNAPSHOT.jar",
+        "tapl.ArithDemo",
+        "../tapl-scala/examples/arith.tapl",
+        "--dump-ast",
+    ];
+    let output = std::process::Command::new("java")
+        .args(&args)
+        .output()
+        .expect("failed to execute process");
+
+    println!("status: {}", output.status);
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    println!("stdout: {}", stdout);
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    assert!(output.status.success());
+
+    let commands: Vec<Command> = serde_json::from_str(&stdout).unwrap();
+    println!("parsed: {:?}", commands);
+
+    for command in commands {
+        match command {
+            EvalCommand {term} => {
+                println!("{:?}", &term);
+                println!("{}", "||");
+                println!("{}", "\\/");
+                let result = big_step_evaluator::eval(*term);
+                println!("{:?}", &result);
+            }
+        }
+    }
 }
--- src/arith.rs
+++ src/arith.rs
@@ -17,6 +17,67 @@ pub enum Command {
     EvalCommand { term: Box<Term> },
 }
 
+pub fn is_numeric_val(t: &Term) -> bool {
+    match t {
+        Term::TmZero {} =>
+            true,
+        Term::TmSucc { term } =>
+            is_numeric_val(term.as_ref()),
+        _ => false,
+    }
+}
+
+pub mod big_step_evaluator {
+    use arith::*;
+    use arith::Term::*;
+
+    // this is a version when we desctruct and starts owning the passed value
+    pub fn eval(t: Term) -> Term {
+
+        match t {
+            TmTrue {} => t,
+            TmFalse {} => t,
+            TmZero {} => t,
+            TmIf { condition, term1, term2 } => match eval(*condition) {
+                TmTrue {} => eval(*term1),
+                TmFalse {} => eval(*term2),
+                _ => panic!("No Rule Applies"),
+            }
+            TmSucc {term} => {
+                let t2 = eval(*term);
+                if is_numeric_val(&t2) {
+                    TmSucc {term : Box::new(t2)}
+                } else {
+                    panic!("No Rule Applies")
+                }
+            }
+            TmPred {term} => {
+                match eval(*term) {
+                    TmZero {} => TmZero {},
+                    TmSucc {term} => if is_numeric_val(&term) {
+                        *term
+                    } else {
+                        panic!("No Rule Applies")
+                    }
+                    _ => panic!("No Rule Applies"),
+                }
+            }
+            TmIsZero {term} => {
+                match eval(*term) {
+                    TmZero {} => TmTrue {},
+                    TmSucc {term} => if is_numeric_val(&term) {
+                        TmFalse {}
+                    } else {
+                        panic!("No Rule Applies")
+                    }
+                    _ => panic!("No Rule Applies")
+                }
+            }
+        }
+
+    }
+}
+
 #[test]
 fn test_serde() {
     let term1 = Term::TmSucc {term: Box::new(Term::TmZero {})};
